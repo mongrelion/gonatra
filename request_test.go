af8a556f58c502862a75417f683686d72ebacdec
Carlos León <mail@carlosleon.info>
2013-09-24 14:24:26 +0400

    Simplifying tests.

--- request_test.go
+++ request_test.go
@@ -6,30 +6,33 @@ import (
 )
 
 func TestBuildRequest(t *testing.T) {
-    route        := Route{"/foo/:id/bar/:bar_id", HTTP_GET, nil, nil}
-    url          := "http://example.com/foo/123/bar/456"
-    request, err := http.NewRequest(HTTP_GET, url, nil)
+    expectations := map[string]string{
+        "id"     : "123",
+        "bar_id" : "456",
+        "tepote" : "foo",
+        "lang"   : "en",
+    }
+    route    := Route{"/foo/:id/bar/:bar_id", HTTP_GET, nil, nil}
+    url      := "http://example.com/foo/123/bar/456?tepote=foo&lang=en"
+    req, err := http.NewRequest(HTTP_GET, url, nil)
     if err != nil {
         t.Errorf("Something went wrong while creating fake request to %s", url)
     }
-    gonatraRequest := buildRequest(request, &route)
+    request := buildRequest(req, &route)
     // Test it has set HttpRequest
-    if gonatraRequest.HttpRequest == nil {
+    if request.HttpRequest == nil {
         t.Errorf("expected HttpRequest to be set but got nil")
     }
 
     // Test it has set Params.
-    if gonatraRequest.Params == nil {
+    if request.Params == nil {
         t.Errorf("expected Params to be set but got nil")
     }  else {
-        // Test it sets the params properly.
-        fooParam := gonatraRequest.Params["id"]
-        barParam := gonatraRequest.Params["bar_id"]
-        if fooParam != "123" {
-            t.Errorf(`expected param "id" to be "123" but got %s`, fooParam)
-        }
-        if barParam != "456" {
-            t.Errorf(`expected param "bar_id" to be "456" but got %s`, barParam)
+        for param, expected := range expectations {
+            actual := request.Params[param]
+            if actual != expected {
+                t.Errorf(`expected param "%s" to be "%s" but got %s`, param, expected, actual)
+            }
         }
     }
 }
@@ -40,25 +43,25 @@ func TestGetParams(t *testing.T) {
     if err != nil {
         t.Errorf("Something went wrong while creating fake request to %s", url)
     }
-    path           := "/users/:id/articles/:article_id/comments/:comment_id"
-    route          := Route{path, HTTP_GET, nil, nil}
-    params         := getParams(&route, request)
-    expectedParams := map[string]string{
-        "id":         "123",
-        "article_id": "456",
-        "comment_id": "789",
-        "foo":        "bar",
-        "lolz":       "katz",
+    path         := "/users/:id/articles/:article_id/comments/:comment_id"
+    route        := Route{path, HTTP_GET, nil, nil}
+    params       := getParams(&route, request)
+    expectations := map[string]string{
+        "id"         : "123",
+        "article_id" : "456",
+        "comment_id" : "789",
+        "foo"        : "bar",
+        "lolz"       : "katz",
     }
-    for key, expectedValue := range expectedParams {
-        val, keyIsPresent := params[key]
+    for param, expected := range expectations {
+        actual, keyIsPresent := params[param]
         // Test that the key is present.
         if !keyIsPresent {
-            t.Errorf("expected key %s to be present in params map.", key)
+            t.Errorf("expected param %s to be present in params map.", param)
         } else {
             // Test that the key holds the proper value.
-            if expectedValue != val {
-                t.Errorf("expected key %s to have value %s but got %s", key, expectedValue, val)
+            if expected != actual {
+                t.Errorf("expected param %s to have value %s but got %s", param, expected, actual)
             }
         }
     }
